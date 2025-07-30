0efa4ae7b0eb47cc4bbd47589ae03f1d0aa0a66f
nurekowser01 <48252197+nurekowser01@users.noreply.github.com>
2025-07-30 16:43:17 +0600

    [Stable]

--- src/app/components/breaker/breaker.component.ts
+++ src/app/components/breaker/breaker.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { Breaker } from '../../models/breaker.model';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-breaker',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatButtonModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule,MatIconModule],
   templateUrl: './breaker.component.html',
   styleUrls: ['./breaker.component.css']
 })
