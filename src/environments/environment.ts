572d6c257db001dffacafe6f1b3f42c7763cc186
nurekowser01 <48252197+nurekowser01@users.noreply.github.com>
2025-08-02 21:07:34 +0600

    test

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  github: {
+    api: 'https://api.github.com',
+    repo: 'nurekowser01/NWPGCL_Drawing',
+    token: '' // Empty for development
+  }
+};
+
