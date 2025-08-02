9a0fd7bff9c412006fb01554bf5fdff0abc022a9
nurekowser01 <48252197+nurekowser01@users.noreply.github.com>
2025-08-02 19:21:51 +0600

    env path deleted

--- src/app/services/drawing.service.ts
+++ src/app/services/drawing.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { map, catchError, shareReplay } from 'rxjs/operators';
-import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
