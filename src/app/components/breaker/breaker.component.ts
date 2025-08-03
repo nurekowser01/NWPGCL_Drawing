3eaaac6638cd56720152483df388a823f894a0c6
nurekowser01 <48252197+nurekowser01@users.noreply.github.com>
2025-08-03 11:55:40 +0600

    test remote

--- src/app/components/breaker/breaker.component.ts
+++ src/app/components/breaker/breaker.component.ts
@@ -1,4 +1,3 @@
-// breaker.component.ts
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
@@ -7,7 +6,8 @@ import { Breaker } from '../../models/breaker.model';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialog } from '@angular/material/dialog';
 import { EditBreakerDialogComponent } from '../edit-breaker/edit-breaker-dialog.component';
-import { DataService } from '../../services/data.service'; // Now using abstract class
+import { DataService } from '../../services/data.service';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-breaker',
@@ -20,6 +20,7 @@ export class BreakerComponent {
   @Input() breaker!: Breaker;
   @Input() busId!: string;
   @Input() panelId!: string;
+  isProduction = environment.production; // Add this line
 
   constructor(
     private dialog: MatDialog,
@@ -27,28 +28,27 @@ export class BreakerComponent {
   ) {}
 
   editBreaker(): void {
-  const dialogRef = this.dialog.open(EditBreakerDialogComponent, {
-    width: '500px',
-    data: { breaker: {...this.breaker} }
-  });
+    const dialogRef = this.dialog.open(EditBreakerDialogComponent, {
+      width: '500px',
+      data: { breaker: {...this.breaker} }
+    });
 
-  dialogRef.afterClosed().subscribe(result => {
-    if (result) {
-      this.dataService.updateBreaker(
-        this.busId,
-        this.panelId,
-        this.breaker.id,
-        result
-      ).subscribe({
-        next: (updatedBreaker) => {
-          this.breaker = updatedBreaker;
-        },
-        error: (error) => {
-          console.error('Update failed:', error);
-          
-        }
-      });
-    }
-  });
-}
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.dataService.updateBreaker(
+          this.busId,
+          this.panelId,
+          this.breaker.id,
+          result
+        ).subscribe({
+          next: (updatedBreaker) => {
+            this.breaker = updatedBreaker;
+          },
+          error: (error) => {
+            console.error('Update failed:', error);
+          }
+        });
+      }
+    });
+  }
 }
