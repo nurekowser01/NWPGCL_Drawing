f7d8fe0c705dcc459aa100fea21c7fccfbd4c5c7
nurekowser01 <48252197+nurekowser01@users.noreply.github.com>
2025-08-03 12:07:26 +0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,10 @@
-// src/environments/environment.prod.ts
+// src/environments/environment.ts
 export const environment = {
-  production: true
+  production: true,
+  github: {
+    api: 'https://api.github.com',
+    repo: 'nurekowser01/NWPGCL_Drawing',
+    token: '' // Empty by default
+  },
+  enableUpdates: false // make this flag false before commit, 
 };
